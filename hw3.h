69112644e4174bc179ed2b2135a62bec797d4972
Anthony Torns <alt175@txstate.edu>
2020-09-21 16:49:13 -0500

    Delete hw3.h

--- hw3.h
+++
@@ -1,9 +0,0 @@
-#ifndef HW3_H
-#define HW3_H
-#include <string>
-using std::string;
-void ReverseArray(int [], int);
-bool HasBalancedParentheses(string);
-bool InDogish(string);
-bool InXish(string, string);
-#endif
